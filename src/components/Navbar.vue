a0fb93fb766d2d7515c449b990f77133fdab42dc
Feryel Khelifi <feryelkhelifi@MacBook-Air-de-Feryel.local>
2026-05-19 16:56:52 +0200

    Performance step up

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,10 @@
     class="border border-black rounded-[2rem] px-6 py-4 flex items-center w-full max-w-7xl mx-auto mt-6 relative z-30"
   >
     <router-link to="/" aria-label="Accueil">
-    <img src="/logo.png" alt="Logo Feryel Khelifi" class="h-10 md:h-12 w-auto m-4" />
+    <img src="/logo.png" alt="Logo Feryel Khelifi" 
+    width="120"
+  height="40"
+  class="h-10 md:h-12 w-auto m-4" />
     </router-link>
 
     <div class="flex-1"></div>
